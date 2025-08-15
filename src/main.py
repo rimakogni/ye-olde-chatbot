dabc62cb79e0e20d462111e957306cab133e1a88
Javier Rivera Martin <javier.rivera.martin@gmail.com>
2025-08-15 13:30:02 +0100

    decode task

--- src/main.py
+++ src/main.py
@@ -4,4 +4,7 @@ from chatbot import Chatbot
 
 bot = Chatbot()
 encoded = bot.encode_prompt("Hello, how are you?")
-print(encoded)
+
+#print(encoded)
+reply = bot.decode_reply([15496, 703, 345, 30]) # Pass in a string of generated token IDs here from your tokenizer
+print(reply)
