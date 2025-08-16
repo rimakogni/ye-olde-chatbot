6c9ebfe50f62f428e4973e726446b7d2ade3940d
Javier Rivera Martin <javier.rivera.martin@gmail.com>
2025-08-16 15:15:13 +0100

    solve Conversation History

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,14 +1,54 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-
+import torch
 class Chatbot:
     def __init__(self, model_name="microsoft/DialoGPT-small"):
 
-
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
-        
-    def encode_prompt(self, prompt: str):
+        self.chat_history_ids = None
+
+    #    output example:
+    #     {
+    #     'input_ids': tensor([[101, 2054, 2003, 1996, 4633, 102]]),
+    #     'attention_mask': tensor([[1, 1, 1, 1, 1, 1]])
+    #     }   
+    def encode_prompt(self, prompt: str) -> dict:
+
         return self.tokenizer(prompt,return_tensors="pt")
-    
+   
+    # input
+    # reply_ids = [101, 2009, 2003, 2157, 1998, 2829, 102]
+    # skip_special_tokens=True
+    #
+    # output example:
+    #     "It is sunny and warm"
     def decode_reply(self, reply_ids: list[int]) -> str:
         return self.tokenizer.decode(reply_ids, skip_special_tokens=True)
+    
+
+    def generate_reply(self, prompt: str) -> str:
+        
+        new = self.encode_prompt(prompt.rstrip() + "\n")
+        new_ids = new["input_ids"] 
+
+        if self.chat_history_ids is None:
+            inputs_ids = new_ids
+        else:
+            inputs_ids = torch.cat([self.chat_history_ids, new_ids], dim=1)
+            
+        input_len = inputs_ids.shape[1]
+        output = self.model.generate(
+            input_ids = inputs_ids,
+            do_sample=True,
+            temperature=0.9,            # less random
+            top_p=0.8,
+            top_k=50,
+            pad_token_id=self.tokenizer.eos_token_id,
+            eos_token_id=self.tokenizer.eos_token_id,
+        )
+        self.chat_history_ids = output
+        return self.tokenizer.decode(output[0, input_len:], skip_special_tokens=True).strip()
+    
+    def reset_history(self):
+        
+        self.chat_history_ids = None
--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ from chatbot import Chatbot
 
 
 bot = Chatbot()
-encoded = bot.encode_prompt("Hello, how are you?")
+# prompt = "What is the weather like today?"
+# reply = bot.generate_reply(prompt)
+# print(f"Prompt: {prompt}")
+# print(f"Reply: {reply}")
 
-#print(encoded)
-reply = bot.decode_reply([15496, 703, 345, 30]) # Pass in a string of generated token IDs here from your tokenizer
-print(reply)
+prompts = [
+    "What's your name?",
+    "What do you think about AI?",
+    "Sorry, tell me your name again."
+]
+
+for prompt in prompts:
+    reply = bot.generate_reply(prompt)
+    print(f"Prompt: {prompt}")
+    print(f"Reply: {reply}\n")
